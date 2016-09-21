e01a1b49a72382682a4bf8359c954c4204f84b6a
Blizzard25 <evansanders@Evans-MacBook-Pro.local>
2016-09-21 02:05:57 -0600

    initial commit
    initial commit

---
+++ LibraryTest/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using YahooFinanceExchangeQueryLibrary;
+using System.Collections.Generic;
+
+namespace LibraryTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public static List<InputCode> InputCodes = new List<InputCode>();
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            InputCodes.Add(new InputCode("USD"));
+            InputCodes.Add(new InputCode("EUR"));
+            InputCodes.Add(new InputCode("GBP"));
+            CurrencyCodeViewModel cm = new CurrencyCodeViewModel(InputCodes);
+            System.Diagnostics.Debug.WriteLine(CurrencyCodeViewModel.InputCodes.ToString());
+            
+        }
+    }
+}
